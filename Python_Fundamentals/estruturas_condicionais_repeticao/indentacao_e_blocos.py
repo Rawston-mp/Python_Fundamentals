cb9e52ae31036740847a80f0d85bd8fbae467610
Rawston-mp <rawstonm@gmail.com>
2024-10-24 23:13:32 -0300

    Identação!!

---
+++ Python_Fundamentals/estruturas_condicionais_repeticao/indentacao_e_blocos.py
@@ -0,0 +1,16 @@
+def sacar(valor):
+    saldo = 500
+    
+    if saldo >= valor:
+        print("valor sacado")
+        print("Retire o dinheiro no caixa!!")
+        
+    print("Obrigado por ser nosso Cliente!!!")
+    
+
+def depositar(valor):
+    saldo = 500
+    saldo += valor        
+        
+        
+sacar(100)
